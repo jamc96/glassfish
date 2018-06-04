39b519976f23dce53915a3b7a9da75dcf728f3a9
Jose Mejia <jamc.mejia@gmail.com>
2018-06-04 13:20:54 -0600

    updating second test case

--- examples/test2.pp
+++ examples/test2.pp
@@ -1,6 +1,22 @@
 # second test case
 #
 #
+# createing user/group glassfish
+group { 'glassfish':
+  gid => '2100',
+}
+user { 'glassfish':
+  ensure  => present,
+  comment => 'Managed by Puppet',
+  home    => '/home/glassfish',
+  shell   => '/bin/bash',
+  uid     => '2100',
+  gid     => '2100',
+  require => Group['glassfish'],
+}
+# adding relationship between user and class
+User['glassfish'] -> Class['glassfish']
+# install, configure and manage service
 class { '::glassfish':
   manage_user => false,
 }
