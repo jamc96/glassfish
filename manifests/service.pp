a72c1bcd6007f3b2e2f2f37669777f5124ed1f79
jamc96 <jamc.mejia@gmail.com>
2018-06-28 08:37:14 -0600

    updating unit testing for glassfish service class

--- manifests/service.pp
+++ manifests/service.pp
@@ -7,19 +7,16 @@
 # @example
 #   include glassfish::service
 class glassfish::service(
-  $service_ensure = $::glassfish::service_ensure,
-  $asadmin_path   = $::glassfish::asadmin_path,
-  $service_name   = $::glassfish::service_name,
-  $domain         = $::glassfish::domain,
-  $port           = $::glassfish::port,
+  $service_name   = 'glassfish',
+  $domain         = 'domain1',
+  $port           = '4848',
 ){
   # create service glassfish
   service { 'glassfish':
-    ensure  => $service_ensure,
+    ensure  => 'running',
     name    => "${service_name}_${domain}",
     start   => "/etc/init.d/${service_name}_${domain} start",
     stop    => "/etc/init.d/${service_name}_${domain} stop",
     restart => "/etc/init.d/${service_name}_${domain} restart",
   }
-
 }
--- spec/classes/service_spec.rb
+++ spec/classes/service_spec.rb
@@ -4,15 +4,15 @@ describe 'glassfish::service' do
   on_supported_os.each do |os, os_facts|
     context "on #{os}" do
       let(:facts) { os_facts }
-
+      # default compilation without dependency cicle
       it { is_expected.to compile }
-      it {
-        is_expected.to contain_service('glassfish').with(
-          'name'      => 'glassfish',
-          'ensure'    => 'running',
-          'enable'    => 'true',
-        )
-      }
-    end
+      # ensure service running 
+      it { is_expected.to contain_service('glassfish').with( 
+        :ensure => 'running',
+        :name    => "glassfish_domain1",
+        :start   => "/etc/init.d/glassfish_domain1 start",
+        :stop    => "/etc/init.d/glassfish_domain1 stop",
+        :restart => "/etc/init.d/glassfish_domain1 restart")}
+    end 
   end
 end
