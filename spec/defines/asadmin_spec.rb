b69fd5c6d93a7d8676bb9a738c7fd2798befef95
jamc96 <jamc.mejia@gmail.com>
2018-07-03 15:59:57 -0600

    adding testing for asadmin defined type

--- spec/defines/asadmin_spec.rb
+++ spec/defines/asadmin_spec.rb
@@ -9,6 +9,35 @@ describe 'glassfish::asadmin' do
       let(:facts) { os_facts }
 
       it { is_expected.to compile }
+      it { is_expected.to compile.with_all_deps }
+      # valide script file
+      it {
+        is_expected.to contain_file('/home/glassfish/configs.sh').with(
+          ensure: 'file',
+          owner: 'root',
+          group: 'root',
+          mode: '0500',
+        )
+      }
+      # file validate_cmd attribute
+      case os_facts[:operatingsystem]
+      when 'CentOS'
+        if os_facts[:operatingsystemmajrelease] == '7'
+          it { is_expected.to contain_file('/home/glassfish/configs.sh').with(validate_cmd: '/usr/bin/sh -n %') }
+        end
+      else
+        it { is_expected.to contain_file('/home/glassfish/configs.sh').with(validate_cmd: '/bin/sh -n %') }
+      end
+      # validate executions
+      it { is_expected.to contain_file('/home/glassfish/configs.sh').that_notifies('Exec[/home/glassfish/configs.sh]') }
+      # script execution
+      it {
+        is_expected.to contain_exec('/home/glassfish/configs.sh').with(
+          command: 'sh /home/glassfish/configs.sh',
+          refreshonly: true,
+          path: '/bin/:/sbin/:/usr/bin/:/usr/sbin/',
+        )
+      }
     end
   end
 end
