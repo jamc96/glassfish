95652323c7a4757f968d416c395edf617793ce63
jamc96 <jamc.mejia@gmail.com>
2018-07-03 13:46:45 -0600

    adding unit testing for defined types

---
+++ spec/defines/create_daemon.rb
@@ -0,0 +1,17 @@
+require 'spec_helper'
+
+describe 'glassfish::create_daemon' do
+  let(:title) { 'tunning' }
+  let(:params) do { 'asadmin_path' => '/tmp'} end
+
+  on_supported_os.each do |os, os_facts|
+    context "on #{os}" do
+      let(:facts) { os_facts }
+
+      it { is_expected.to compile }
+      it { is_expected.to compile.with_all_deps}
+
+      it { is_expected.to contain_file('/etc/init.d/glassfish_domain1').with( ensure: 'present', mode: '0775')}
+    end
+  end
+end
